7fb213b08ea848a793d10e5441fa8ac31409a3c0
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2021-11-01 21:55:10 +0530

    removed animate presence animations

--- pages/_app.js
+++ pages/_app.js
@@ -4,35 +4,35 @@ import Navbar from "../components/layout/Navbar";
 import Container from "../components/layout/Container";
 import { AnimatePresence, motion } from "framer-motion";
 
-const variants = {
-  hidden: { opacity: 0, x: 0, y: 20 },
-  enter: { opacity: 1, x: 0, y: 20 },
-  exit: { opacity: 0, x: 0, y: 20 },
-};
+// const variants = {
+//   hidden: { opacity: 0, x: 0, y: 20 },
+//   enter: { opacity: 1, x: 0, y: 20 },
+//   exit: { opacity: 0, x: 0, y: 20 },
+// };
 
-const AnimateLayout = ({ children }) => (
-  <motion.div
-    initial="hidden"
-    animate="enter"
-    exit="exit"
-    variants={variants}
-    transition={{ duration: 0.4, type: "easeInOut" }}
-    style={{ position: "relative" }}
-  >
-    {children}
-  </motion.div>
-);
+// const AnimateLayout = ({ children }) => (
+//   <motion.div
+//     initial="hidden"
+//     animate="enter"
+//     exit="exit"
+//     variants={variants}
+//     transition={{ duration: 0.4, type: "easeInOut" }}
+//     style={{ position: "relative" }}
+//   >
+//     {children}
+//   </motion.div>
+// );
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <ThemeProvider attribute="class" enableSystem={false}>
       <AnimatePresence exitBeforeEnter initial={true}>
           <Navbar />
-        <AnimateLayout>
+        {/* <AnimateLayout> */}
           <Container>
             <Component {...pageProps} key={router.route} />
           </Container>
-        </AnimateLayout>
+        {/* </AnimateLayout> */}
       </AnimatePresence>
     </ThemeProvider>
   );
---
+++ pages/contact.js
@@ -0,0 +1,20 @@
+function contact() {
+  return (
+    <div className='p-4'>
+       <h1 className="font-bold text-center sm:text-left text-2xl leading-loose tracking-wider uppercase mb-4">
+        Connect With Me
+      </h1>
+      
+    </div>
+  )
+}
+
+export default contact
+
+const TwitterIcon = (...props) => {
+
+}
+
+const LinkedInIcon = (...props) => {
+
+}
